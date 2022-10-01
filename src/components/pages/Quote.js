2f28567fd7eab3b0dd0b1c0f23e4f79f6c598c04
normainobary2021 <norman.ainobushoborozi@gmail.com>
2022-10-01 22:47:40 +0300

    Remove more eslint errors

--- src/components/pages/Quote.js
+++ src/components/pages/Quote.js
@@ -1,10 +1,6 @@
 const Quote = () => (
   <main className="quote-main">
-    <p>
-    Software testing is a sport like hunting, it&apos;s bughunting. -
-    {' '}
-    <span className="cite">Microverse</span>
-    </p>
+      <p>Software testing is a sport like hunting, it&apos;s bughunting.<span className="cite">Microverse</span></p>
   </main>
 );
 export default Quote;
